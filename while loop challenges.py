48cd7e43e8ee7944e71bbe4703f86d33b0f4f7aa
Leoluis0710 <98952616+Leoluis0710@users.noreply.github.com>
2022-03-03 12:23:15 +0000

    Add files via upload

--- while loop challenges.py
+++ while loop challenges.py
@@ -1,5 +1,5 @@
-total = 0
-while total <=50:
-    number = int(input('input a number'))
-    total = total + number
-    print('the total is',total)
+#while loop challenges.
+number = 0
+while number < 5:
+    number = int(input('enter a number'))
+    print('the last number you entered was',number)
